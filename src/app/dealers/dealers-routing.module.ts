463b9230a2cf5c7d20bf580f5e7e5c10c0d4ac44
jassmanni <jassmanniq@gmail.com>
2021-07-02 20:25:46 -0400

    fix dealers routes. fix links for create and edit in dealers

--- src/app/dealers/dealers-routing.module.ts
+++ src/app/dealers/dealers-routing.module.ts
@@ -1,28 +1,39 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { DealersListComponent } from '../dealers-list/dealers-list.component';
+import { DealersListComponent } from './dealers-list/dealers-list.component';
 import { DealerDetailComponent } from './dealer-detail/dealer-detail.component';
 import { DealerEditComponent } from './dealer-edit/dealer-edit.component';
+import { DealersComponent } from './dealers.component';
 import { NewDealerComponent } from './new-dealer/new-dealer.component';
 
 const routes: Routes = [
   {
-    path: 'new-dealer', component: NewDealerComponent
+    path: '',
+    component: DealersComponent,
+    children: [
+      {
+        path: 'new-dealer',
+        component: NewDealerComponent,
+      },
+      {
+        path: 'list',
+        component: DealersListComponent,
+      },
+      {
+        path: ':id',
+        component: DealerDetailComponent,
+      },
+      {
+        path: 'edit/:id',
+        component: DealerEditComponent,
+      },
+      { path: '', redirectTo: 'list' },
+    ],
   },
-  {
-    path: 'list', component: DealersListComponent
-  },
-  {
-    path: ':id', component: DealerDetailComponent
-  },
-  {
-    path: 'edit/:id', component: DealerEditComponent
-  }
-
 ];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class DealersRoutingModule { }
+export class DealersRoutingModule {}
