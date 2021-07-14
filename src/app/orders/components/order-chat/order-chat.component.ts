817c597b1ce1c6b651d9e78cf80a21d06d0aa5fc
jassmanni <jassmanniq@gmail.com>
2021-07-14 11:29:09 -0400

    order component now render messages from db

--- src/app/orders/components/order-chat/order-chat.component.ts
+++ src/app/orders/components/order-chat/order-chat.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { switchMap, tap } from 'rxjs/operators';
+import { Message } from 'src/app/models/message';
+import { MessageService } from 'src/app/services/message.service';
 
 @Component({
   selector: 'app-order-chat',
@@ -6,7 +10,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./order-chat.component.scss'],
 })
 export class OrderChatComponent implements OnInit {
-  constructor() {}
+  orderId: string;
+  messages: Message[] = [];
+  constructor(
+    private messageService: MessageService,
+    private route: ActivatedRoute
+  ) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.route.params
+      .pipe(
+        tap(({ id }) => (this.orderId = id)),
+        switchMap(({ id }) => this.messageService.getMessages(id))
+      )
+      .subscribe((messages) => (this.messages = messages));
+  }
 }
