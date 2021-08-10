211277ae359bc06b177862af39af36994d744f1a
jassmanni <jassmanniq@gmail.com>
2021-08-10 06:01:46 -0400

    colors change in home

--- src/app/pipes/pipes.module.ts
+++ src/app/pipes/pipes.module.ts
@@ -3,10 +3,16 @@ import { CommonModule } from '@angular/common';
 import { DomSanitizerPipe } from './dom-sanitizer.pipe';
 import { FilterPipe } from './filter.pipe';
 import { FilterCategoriesPipe } from './filter-categories.pipe';
+import { UpperFirstPipe } from './upper-first.pipe';
 
 @NgModule({
-  declarations: [DomSanitizerPipe, FilterPipe, FilterCategoriesPipe],
-  exports: [DomSanitizerPipe, FilterPipe, FilterCategoriesPipe],
+  declarations: [
+    DomSanitizerPipe,
+    FilterPipe,
+    FilterCategoriesPipe,
+    UpperFirstPipe,
+  ],
+  exports: [DomSanitizerPipe, FilterPipe, FilterCategoriesPipe, UpperFirstPipe],
   imports: [CommonModule],
 })
 export class PipesModule {}
